0c96951068e7db2e3bb45412762de6c55a81deb4
Your Name <hafizhaseeb07@gmail.com>
2022-05-03 13:13:15 +0200

    stopwatch

--- index.js
+++ index.js
@@ -1,43 +1,8 @@
- function Stopwatch() {
-     let startTime, endTime, running, duration = 0;
+const numbers = [1,2,3,4,5];
 
-     this.start = function () {
-         if (running) {
-             throw new Error('Already Running');
-         }
-        else{
-             running = true;
-             startTime = new Date();
-         }
 
-     }
-     this.stop = function () {
-         if (!running) {
-             throw new Error('stopwatch is not started');
-            }
-        else{
-             running = false;
 
-             endTime = new Date();
+const sum = numbers.reduce((a, b)=> a + b
+);
 
-             const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
-             duration += seconds;
-         }
-
-     }
-     this.reset = function () {
-         startTime = null;
-         endTime = null;
-         running = false;
-         this.duration = 0;
-     }
-     Object.defineProperty(this, 'duration', {
-         get: function () {
-             duration = endTime - startTime;
-             return duration;
-         }
-     });
- }
-
- const sw = new Stopwatch();
- console.log(sw.duration);
+console.log();
